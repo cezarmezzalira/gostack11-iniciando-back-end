c3273e44f75e3c7bbc649eee5a47dbeabe5fe659
Cezar Augusto Mezzalira <cezar.mezzalira@gmail.com>
2021-01-08 01:05:41 -0300

    DDD Paths Changes

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,10 +1,10 @@
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
-import AppError from '../errors/AppError';
+import AppError from '@shared/errors/AppError';
 
-import Appointment from '../models/Appointment';
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
 
-import AppointmentsRepository from '../repositoty/AppointmentsRepository';
+import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 
 interface Request {
   date: Date;
